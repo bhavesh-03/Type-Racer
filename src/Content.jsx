347449fc6afd34bf4512b9b4613b5bebc1744237
Bhavesh Mankar <bmmankar25@gmail.com>
2023-09-28 11:03:26 +0530

    RaceTrack Added

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import RaceTrack from "./" ;
-import TextCard from "./"
+import RaceTrack from "./components/RaceTrack" ;
+import TextCard from "./components/"
 
 const Content = ({text}) => {
-
+    const [wpm, setWpm] = useState(0);
+    console.log(wpm);
+    const [percentComplete, setPercentComplete] = useState(0);
+    const [started, setStarted] = useState(false);
     const startAgain = () =>{
         window.location.reload() ;
     }
@@ -11,7 +14,18 @@ const Content = ({text}) => {
   return (
     <div>
         <button onClick={startAgain}>Start Again</button>
-
+        <RaceTrack
+                wpm={wpm}
+                percentComplete={percentComplete}
+                started={started}
+        />
+        <TextCard
+                textToType={text}
+                setWpm={setWpm}
+                setPercentComplete={setPercentComplete}
+                started={started}
+                setStarted={setStarted}
+        />
         
     </div>
   )
